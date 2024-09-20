a51c61ac1fcd618de167128ea94f71bcb6e8bbc4
s1zykh <vladik.sizykh21@mail.ru>
2024-09-21 00:20:35 +0300

    update Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Button appearance="ghost" arrow="down">
+          Кнопка
+        </Button>
+      </main>
       <footer className={styles.footer}></footer>
     </div>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,18 +3,23 @@ import React, {
   DetailedHTMLProps,
   ReactNode,
 } from "react";
+import Image from "next/image";
+
 import cn from "classnames";
+import Arrow from "../../../public/assets/arrow.svg";
 import styles from "./Button.module.scss";
 
 type AppearanceType = "primary" | "ghost";
+type ArrowType = "right" | "down" | "none";
 
 interface ButtonProps
   extends DetailedHTMLProps<
     ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   > {
-  children?: ReactNode;
+  children: ReactNode;
   appearance: AppearanceType;
+  arrow?: ArrowType;
 }
 const clsx = (appearance: AppearanceType) => {
   return cn(styles.button, {
@@ -23,6 +28,30 @@ const clsx = (appearance: AppearanceType) => {
   });
 };
 
-export const Button = ({ children, appearance }: ButtonProps): JSX.Element => {
-  return <button className={clsx(appearance)}>{children}</button>;
+const ButtonContent = (children: ReactNode, arrow: ArrowType) => {
+  if (children) {
+    return (
+      <>
+        {children}
+        {arrow !== "none" && (
+          <span
+            className={cn(styles.arrow, {
+              [styles.down]: arrow == "down",
+            })}
+          >
+            <Image src={Arrow} alt="arrow" />
+          </span>
+        )}
+      </>
+    );
+  }
+};
+
+export const Button = (props: ButtonProps): JSX.Element => {
+  const { children, appearance, arrow = "none" } = props;
+  return (
+    <button className={clsx(appearance)}>
+      {ButtonContent(children, arrow)}
+    </button>
+  );
 };
